acf11c60e7451461aef1513f7a174112cafd676c
Ha72 <phuonglh72@gmail.com>
2020-11-03 17:03:10 -0600

    Added _customer view partial

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register Customer do
 
-  permit_params :name, :phone, :email, :image, :notes  
+  permit_params :name, :phone, :email, :image, :notes
+  
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    
+    # f.inputs do
+    #   f.input :image, as: :file
+    # end
+    
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+    # has_one_attached :image
+    
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
