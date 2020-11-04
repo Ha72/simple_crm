c1d4f4594ddb78982925ccfc9fcaab3194b16197
Ha72 <phuonglh72@gmail.com>
2020-11-03 21:24:59 -0600

    Created Customer controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  root to: "admin/dashboard#index"
+  root to: "customers#index"
   
-  resources :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/alphabetized", to: "customers#alphabetized"
+  
+  get "/missingemail", to: "customers#missing_email"
+  
 end
